20c8142ba329dabde2d44d23833ac49c92422731
hadeersalah7 <hadeermhmd70@gmail.com>
2024-09-17 15:15:21 +0300

    adding dynamic lists instead of hard coded ones

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,38 @@
 import React from 'react'
-
+import logo from "../images/logo.svg"
+import { pagesData, pagesLinks } from './data'
 const Navbar = () => {
     return (
         <>
             <nav className="navbar">
                 <div className="nav-center">
                     <div className="nav-header">
-                        <img src="./images/logo.svg" className="nav-logo" alt="backroads" />
+                        <img src={logo} className="nav-logo" alt="backroads" />
                         <button type="button" className="nav-toggle" id="nav-toggle">
-                            <i className="fas fa-bars"></i>
+                            <i className="fas fa-bars" />
                         </button>
                     </div>
                     {/* <!-- left this comment on purpose --> */}
                     <ul className="nav-links" id="nav-links">
-                        <li>
-                            <a href="#home" className="nav-link"> home </a>
-                        </li>
+                        {pagesData.map((data) => {
+                            return (
+                                <li key={data.id}>
+                                    <a href={data.href} className='nav-link'>{data.title}</a>
+                                </li>
+                            )
 
-                        <li>
-                            <a href="#about" className="nav-link"> about </a>
-                        </li>
-
-                        <li>
-                            <a href="#services" className="nav-link"> services </a>
-                        </li>
-
-                        <li>
-                            <a href="#tours" className="nav-link"> tours</a>
-                        </li>
+                        })}
                     </ul>
 
                     <ul className="nav-icons">
-                        <li>
-                            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-                            ><i className="fab fa-facebook"></i
-                            ></a>
-                        </li>
-                        <li>
-                            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-                            ><i className="fab fa-twitter"></i
-                            ></a>
-                        </li>
-                        <li>
-                            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-                            ><i className="fab fa-squarespace"></i
-                            ></a>
-                        </li>
+                        {pagesLinks.map((link) => {
+                            return (
+                                <li key={link.href}>
+                                    <a href={link.href} target="_blank" className="nav-icon"
+                                    ><i className={link.icon} /></a>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
             </nav>
---
+++ src/components/data.js
@@ -0,0 +1,12 @@
+export const pagesData = [
+    { id: 1, href: "#home", title: "home" },
+    { id: 1, href: "#about", title: "about" },
+    { id: 1, href: "#sevices", title: "sevices" },
+    { id: 1, href: "#tours", title: "tours" },
+]
+
+export const pagesLinks = [
+    { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
+    { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+    { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace"}
+]
